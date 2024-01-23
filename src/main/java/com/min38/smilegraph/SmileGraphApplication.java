84296deac84ec6513e14bc64810640e11a14e090
min-38 <mindino03@gmail.com>
2024-01-23 16:58:55 +0900

    first commit

---
+++ src/main/java/com/min38/smilegraph/SmileGraphApplication.java
@@ -0,0 +1,13 @@
+package com.min38.smilegraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmileGraphApplication.class, args);
+    }
+
+}
